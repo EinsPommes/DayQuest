e7ae4b306c871677806138b45b09995e34c873ad
EinsPommes <jannikkugler2006@gmail.com>
2024-11-30 14:14:40 +0100

    Initial Commit

---
+++ DayQuestTests/DayQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DayQuestTests.swift
+//  DayQuestTests
+//
+//  Created by Jannik Kugler on 30.11.24.
+//
+
+import Testing
+@testable import DayQuest
+
+struct DayQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
